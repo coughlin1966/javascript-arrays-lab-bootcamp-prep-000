6d581893d2e9a0caa72e4dfcba7658049562f9de
Peter Coughlin <coughlin1966@gmail.com>
2017-11-24 15:15:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 
 var kittens = ['fred', 'alice', 'shelby']
 function destructivelyAppendKitten (kitten) {
-  push (kittens, kitten)
+  kittens.push(kitten)
   return kittens
 }
 destructivelyAppendKitten(Bill)
